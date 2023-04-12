6d9aaeb84677f59db60637f4e2b78f735af01ab6
Max Elbo <hello.maxelbo@gmail.com>
2023-04-13 05:30:38 +0900

    feat(createParish): add createRecords

--- pages/api/createParish.js
+++ pages/api/createParish.js
@@ -8,21 +8,62 @@ console.log({ table });
 
 export default async function createParish(req, res) {
   console.log({ req });
-  const findParishRecords = await table
-    .select({
-      filterByFormula: `id="0"`,
-    })
-    .firstPage();
-  if (findParishRecords.length > 0) {
-    const records = findParishRecords.map((record) => {
-      return {
-        ...record.fields,
-      };
-    });
-    res.status(200).json({ records, message: "Parish already exists" });
-  } else {
+  if (req.method === "POST") {
+    const { id, name, address, ward, distance, imgUrl, votes } = req.body;
     try {
-      res.status(200).json({ message: "create a record" });
+      const findParishRecords = await table
+        .select({
+          filterByFormula: `id="${id}"`,
+        })
+        .firstPage();
+      if (findParishRecords.length > 0) {
+        const records = findParishRecords.map((record) => {
+          return {
+            ...record.fields,
+          };
+        });
+        res
+          .status(200)
+          .json({ message: "This parish already exists", records });
+      } else {
+        if (!id || !name || !address || !ward || !distance || !imgUrl) {
+          res.status(400).json({
+            message: "Missing one or more required fields",
+          });
+          return;
+        }
+        const createRecords = table.create(
+          [
+            {
+              fields: {
+                id,
+                name,
+                address,
+                ward,
+                distance,
+                imgUrl,
+                votes,
+              },
+            },
+          ],
+          function (err, record) {
+            if (err) {
+              console.error(err);
+              return;
+            }
+            console.log(record.getId());
+          }
+        );
+        // const records = createRecords.map((record) => {
+        //   return {
+        //     ...record.fields,
+        //   };
+        // });
+        res.status(200).json({
+          message: "Created a new parish",
+          // records
+        });
+      }
     } catch (err) {
       res.status(500).json({ statusCode: 500, message: err.message });
     }
