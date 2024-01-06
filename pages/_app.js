c64d94c2b53fdc8d73f55093051bd9796ac39547
Max Elbo <hello.maxelbo@gmail.com>
2024-01-06 19:38:13 +0900

    fix(_app): fix typo in footer import

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import ParishProvider from "../context/parish-provider";
-import Footer from "../components/Footer";
+import Footer from "../components/footer";
 import "../styles/globals.css";
 
 export default function MyApp({ Component, pageProps }) {
